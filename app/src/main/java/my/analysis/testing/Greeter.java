cfffd430ac0ab9ce7105af2570936d81925724f6
Daniel Wellman <dan@danielwellman.com>
2021-02-11 15:27:57 -0600

    add 4

--- app/src/main/java/my/analysis/testing/Greeter.java
+++ app/src/main/java/my/analysis/testing/Greeter.java
@@ -19,7 +19,7 @@ public class Greeter {
     }
 
     private int age(String greeting, String name) {
-        return 3;
+        return 4;
     }
 
     private String calculateSalutation(String greeting) {
