e85567d1d06596ac2452ed181641c80c1f852843
Daniel Wellman <dan@danielwellman.com>
2021-02-11 14:22:01 -0600

    Add default null check

--- app/src/main/java/my/analysis/testing/Greeter.java
+++ app/src/main/java/my/analysis/testing/Greeter.java
@@ -1,10 +1,15 @@
 package my.analysis.testing;
 
 public class Greeter {
+    private static final String DEFAULT = "Aloha";
+
     public Greeter() {
     }
 
     public String getGreeting(String greeting, final String name) {
+        if (null == greeting) {
+            greeting = DEFAULT;
+        }
         return greeting + " " + name + "!";
     }
 }
