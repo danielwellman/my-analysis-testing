b6313272f544ca36fabdd33eb154809bc18542f4
Daniel Wellman <dan@danielwellman.com>
2021-02-11 13:33:25 -0600

    Introduce greet method

--- app/src/main/java/my/analysis/testing/App.java
+++ app/src/main/java/my/analysis/testing/App.java
@@ -4,11 +4,15 @@
 package my.analysis.testing;
 
 public class App {
+
+    public void greet() {
+        System.out.println(getGreeting());
+    }
     public String getGreeting() {
         return "Hello World!";
     }
 
     public static void main(String[] args) {
-        System.out.println(new App().getGreeting());
+        new App().greet();
     }
 }
