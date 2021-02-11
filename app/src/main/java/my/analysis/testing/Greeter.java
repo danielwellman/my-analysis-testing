92780438ba3ace35bafef5bf4bddaf9451857e7a
Daniel Wellman <dan@danielwellman.com>
2021-02-11 15:28:48 -0600

    number update

--- app/src/main/java/my/analysis/testing/Greeter.java
+++ app/src/main/java/my/analysis/testing/Greeter.java
@@ -19,7 +19,7 @@ public class Greeter {
     }
 
     private int age(String greeting, String name) {
-        return 9;
+        return 10;
     }
 
     private String calculateSalutation(String greeting) {
